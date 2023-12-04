99ac238fbfd88ac763f93dad82448d3c2f38a5b1
shamal <shamalsundar2000@gmail.com>
2023-12-04 17:17:13 +0530

    small updation

--- Car rental/src/pages/Discount.jsx
+++ Car rental/src/pages/Discount.jsx
@@ -21,7 +21,7 @@ function Discount() {
   <div className='image-container'>
     <img src='https://www.evmwheels.com/front-theme/images/special_offer_2.png' alt="Special Offer 2" />
     <span>
-      <h4>GET 15%OFF ON 3</h4>
+      <h4>GET 15%OFF ON 5</h4>
       <h4>DAYS BOOKING</h4>
     </span>
   </div>
