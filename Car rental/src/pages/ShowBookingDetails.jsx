5b0276a07dcbcd0136092776fc331fe56bf3aa4e
shamal <shamalsundar2000@gmail.com>
2024-02-08 20:47:59 +0530

    changes in show booking details section

--- Car rental/src/pages/ShowBookingDetails.jsx
+++ Car rental/src/pages/ShowBookingDetails.jsx
@@ -3,6 +3,7 @@ import DefaultLayout from "../components/DefaultLayout";
 import Footer from "./Footer";
 import Navbar from "./Navigationbar";
 import Socialmedia from "./Socialmedia";
+import Carousel from 'react-bootstrap/Carousel';
 
 function ShowBookingDetails() {
   const { currentBooking } = useSelector((state) => state.booking);
@@ -35,7 +36,6 @@ function ShowBookingDetails() {
               <p>
                 <strong>Pickup Date:</strong>{" "}
                 {new Date(currentBooking?.detailsAdd?.pickupDate).toLocaleDateString()}
-              
               </p>
               <p>
                 <strong>Pickup Time:</strong>{" "}
@@ -49,8 +49,6 @@ function ShowBookingDetails() {
               <p>
                 <strong>Drop Off Date:</strong>{" "}
                 {new Date(currentBooking?.detailsAdd?.dropOffDate).toLocaleDateString()}
-
-               
               </p>
               <p>
                 <strong>Drop Off Time:</strong>{" "}
@@ -62,17 +60,29 @@ function ShowBookingDetails() {
           <div className="newnav">
             <Navbar />
             <div className="pickupmain">
-              <img
-                style={{
-                  position: "relative",
-                  left: "30px",
-                  bottom: "12px",
-                  width: "1000px",
-                  height: "500px",
-                }}
-                src="https://images.pexels.com/photos/120049/pexels-photo-120049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                alt="benz"
-              />
+              <Carousel>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src="https://images.pexels.com/photos/120049/pexels-photo-120049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    alt="First slide"
+                  />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src="https://images.pexels.com/photos/1335077/pexels-photo-1335077.jpeg?auto=compress&cs=tinysrgb&w=600"
+                    alt="Second slide"
+                  />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src="https://images.pexels.com/photos/112452/pexels-photo-112452.jpeg?auto=compress&cs=tinysrgb&w=600"
+                    alt="Third slide"
+                  />
+                </Carousel.Item>
+              </Carousel>
             </div>
           </div>
         </div>
